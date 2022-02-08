0bf669343f43d2c1275910c871903552c4b36818
Valery <valerydev123@gmail.com>
2022-02-08 15:21:54 +0200

    navigation

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -12,11 +12,14 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
@@ -30,63 +33,72 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val message = remember{ mutableStateOf("") }
-            val stringList = remember { mutableStateListOf<String>().also {
-                it.addAll(stringLst)
-            } }
+            val navController = rememberNavController()
 
-            Box(modifier = Modifier.fillMaxSize()
-                .padding(5.dp)) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    InputTextWidget(message)
-                    ButtonAdd(message = message, stringList)
-                    StringList(stringList)
+            NavHost(navController = navController, startDestination = "listScreen") {
+               composable("listScreen") { ListScreen(stringLst, navController) }
+               composable("selectedItem") { SelectedItem() }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListScreen(stringLst: ArrayList<String>, navController: NavHostController) {
+    
+    val message = remember{ mutableStateOf("") }
+    val stringList = remember { mutableStateListOf<String>().also {
+        it.addAll(stringLst)
+    } }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(5.dp)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = message.value,
+                placeholder = { Text("Enter text") },
+                leadingIcon = { Icon(Icons.Filled.Send, contentDescription = "send") },
+                shape = MaterialTheme.shapes.medium.copy(CornerSize(100.dp)),
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Red,
+                    cursorColor = Color.Black,
+                    focusedIndicatorColor = Color.Green
+                ),
+                onValueChange = { newText -> message.value = newText },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+
+            Button(
+                modifier = Modifier.padding(5.dp),
+                onClick = {
+                    stringList.add(message.value)
                 }
+            ) {
+                Text(text = "Add To List")
             }
 
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                itemsIndexed(stringList) { index, item ->
+                    Text(text = item,
+                        modifier = Modifier.clickable {
+                            navController.navigate("selectedItem")
+                            println("print____${stringList[index]}")
+                        }
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun InputTextWidget(message: MutableState<String>) {
-    OutlinedTextField(
-        value = message.value,
-        placeholder = { Text("Enter text") },
-        leadingIcon = { Icon(Icons.Filled.Send, contentDescription = "send") },
-        shape = MaterialTheme.shapes.medium.copy(CornerSize(100.dp)),
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = Color.Red,
-            cursorColor = Color.Black,
-            focusedIndicatorColor = Color.Green
-        ),
-        onValueChange = { newText -> message.value = newText },
-        modifier = Modifier
-            .fillMaxWidth()
-    )
-}
-
-@Composable
-fun ButtonAdd(message: MutableState<String>, stringList: SnapshotStateList<String>) {
-    Button(
-        modifier = Modifier.padding(5.dp),
-        onClick = {
-            stringList.add(message.value)
-        }
+fun SelectedItem() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(text = "Add To List")
-    }
-}
-
-@Composable
-fun StringList(stringList: SnapshotStateList<String>) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        itemsIndexed(stringList) { index, item ->
-            Text(text = item,
-                 modifier = Modifier.clickable {
-                    println("print____${stringList[index]}")
-                 }
-            )
-        }
+        Text(text = "TEst TEXT")
     }
 }
